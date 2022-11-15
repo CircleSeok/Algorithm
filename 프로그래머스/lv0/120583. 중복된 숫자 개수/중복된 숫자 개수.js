917db62409ba11962a48741982e61d07eccbbb30
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-11-15 16:05:31 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -0,0 +1,17 @@
+function solution(array, n) {
+  let result = array.filter(array => array === n).length;
+    return result
+}
+
+// [array] => [array] === n
+
+
+function solution(array, n) {
+    let count = 0;
+    for(let i = 0; i < array.length; i++) {
+        if(n === array[i]) {
+            count ++
+        }
+    }
+    return count
+}
