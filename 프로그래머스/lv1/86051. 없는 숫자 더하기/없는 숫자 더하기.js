c657e33bc6e0e40db0e60a1891de5659923d9ca6
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-11-15 20:37:54 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,3 +1,14 @@
 function solution(numbers) {
   return  45 - numbers.reduce((a, b) => a + b,0);
 }
+
+
+function solution(numbers) {
+    let answer = 0;
+
+    for(let i = 0; i <= 9; i++) {
+        if(!numbers.includes(i)) answer += i;
+    }
+
+    return answer;
+}
