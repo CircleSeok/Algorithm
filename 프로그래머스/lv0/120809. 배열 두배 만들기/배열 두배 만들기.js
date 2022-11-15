f8592959228c7e03c9bbc8c7b6cd4964861470de
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-11-15 19:42:15 +0900

    [level 0] Title: 배열 두배 만들기, Time: 2.11 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -9,7 +9,10 @@ function solution(numbers) {
 function solution(numbers) {
     let arr = numbers.map(number => number *2)
     return arr
+}
 
-
-
+function solution(numbers) {
+    return numbers.reduce((a, b) => [...a, b * 2], []);
+    // ... a = b 에서 곱한 값들을 담을 곳 a로 하면 배열안에 배열이 계속 쌓여서 ... 을 사용해 펼쳐준다
+    // , [] 위치는 결과값을 받을 형태 이번 문제는 초기값을 [] 배열로 받으니 []로 쓴다 합산 값이면 0
 }
