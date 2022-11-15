8c3c3c3a267a0f3b45589a6b05d5c15cc4bbbf48
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-11-15 16:17:54 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -4,3 +4,24 @@ function solution(num_list) {
     let hol = num_list.length - Zak
     return [Zak, hol]
 }
+
+function solution(num_list) {
+        let answer = [0, 0]
+    for(i = 0; i < num_list.length; i++ ){
+        if(num_list[i] % 2 === 0 ){
+            answer[0]++ 
+        }else{
+            answer[1]++
+        }
+    }
+    return answer
+}
+
+function solution(num_list,countA=0,countB=0,answer = []) {
+
+    for(let i of num_list)
+        {
+            i%2 === 0 ? countA++ : countB++
+        }
+    return [countA,countB];
+}
