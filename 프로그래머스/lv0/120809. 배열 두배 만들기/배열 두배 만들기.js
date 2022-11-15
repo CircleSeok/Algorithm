50988986595e5135942302269b1e1bb4f5d1468d
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-11-15 17:46:01 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.06 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,7 +1,12 @@
 function solution(numbers) {
-  let answer= [];
-    for(i = 0; i < numbers.length; i++){
-        answer.push(numbers[i]*2)
-    }
+    let answer = numbers.reduce((x, y)=> (x, y*2), []);
     return answer
+}
+
+function solution(numbers) {
+    let arr = numbers.map(number => number *2)
+    return arr
+
+
+
 }
