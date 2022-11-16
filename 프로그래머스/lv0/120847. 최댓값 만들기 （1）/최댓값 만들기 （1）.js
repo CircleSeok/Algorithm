3c8324e228381d64b082bb8fc0d40839a7993f59
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-11-16 17:38:21 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,4 +1,11 @@
 function solution(numbers) {
-    var answer = 0;
-    return answer;
+    numbers.sort((a,b)=>b-a);
+    return numbers[0]*numbers[1];
 }
+
+// [1, 2, 3, 4, 5]
+
+// sort 쓰면 스트링 순으로 반환 해줌
+
+// ((a,b)=>b-a)  하면 -로 나와서 큰수 부터나온다.
+// ((a,b)=>a-b) 하면 작은순서대로 나온다.
