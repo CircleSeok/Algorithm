87e5ff1257e276305834725e4e6e792c47f9bf3b
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-11-16 17:57:42 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -3,6 +3,17 @@ function solution(numbers) {
     return numbers[0]*numbers[1];
 }
 
+function solution(numbers) {
+    let mx1 = 0
+    let mx2 = 0
+    let idx = 0
+    mx1 = Math.max(...numbers) //number = [1,2,3,4,5] mx1 =5
+    idx = numbers.indexOf(mx1) //mx1의 인덱스부터 1개를 지운다 number =[1,2,3,4]
+    numbers.splice(idx,1) // mx2 = 4
+    mx2 = Math.max(...numbers) //5 * 4
+    return mx1 * mx2
+}
+
 // [1, 2, 3, 4, 5]
 
 // sort 쓰면 스트링 순으로 반환 해줌
