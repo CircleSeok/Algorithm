cb3b992f0ee014100120b31be72bdb87f1869bee
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-11-17 12:20:23 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -12,10 +12,8 @@ function solution(slice, n) {
 function solution(slice, n) {
     var answer = 0;
     for (let i = 0; i <= n; i++) {
-        if (i < n) {
             if (i % slice === 0) {
                 answer ++
-            }
         }
     }
     return answer;
