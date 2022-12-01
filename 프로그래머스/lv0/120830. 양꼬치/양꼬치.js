34aca822b677e500b260891c9feed5df29095dfc
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-12-01 10:57:48 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,3 +1,10 @@
 function solution(n, k) {
-  return n * 12000 + (k - Math.floor(n / 10)) * 2000;
+  // return n * 12000 + (k - Math.floor(n / 10)) * 2000;
+    return n*12000 + k*2000 -Math.floor(n/10) * 2000 
 }
+
+// 10n 을 먹으면 음료수 k 줌
+// n = 12000 k= 2000
+
+// 서비스 : -Math.floor(n/10) * 2000 
+// n*12000 + k*2000 -Math.floor(n/10) * 2000 
