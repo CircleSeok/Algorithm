bb12f8f5c60b312495edeaae29819376f27e9acd
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-12-01 11:48:24 +0900

    [level 0] Title: 배열의 평균값, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -5,3 +5,7 @@ function solution(numbers) {
         {sum += numbers[i];} // 배열의 요소들 하나씩 더하기
     return answer = sum / numbers.length; // 더한 값과 배열의 길이 나누기
 }
+
+//평균값
+// 전체 숫자를 더하고 그 값을sum 전체 숫자의 갯수로numbers.length 나눈것
+// numbers[0]
