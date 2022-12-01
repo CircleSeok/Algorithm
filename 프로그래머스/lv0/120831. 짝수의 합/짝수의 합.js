3ccc57939ca6eb34481e649419478f1f511e1875
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-12-01 11:35:31 +0900

    [level 0] Title: 짝수의 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,10 +1,18 @@
 function solution(n) {
     let answer = 0;
     
-    for(let i = 0; i <= n; i ++){
+    for(let i = 1; i <= n; i ++){
         if(i % 2 === 0){
             answer += i;
         }
     }
     return answer;
 }
+
+// ++x 먼저 더해줘 그리고 나와 
+// x++ 나오고 추가해줘
+
+// += 더하고 저장해줘  sum = sum + i
+// = sum = i 할당
+// -=
+// *=
