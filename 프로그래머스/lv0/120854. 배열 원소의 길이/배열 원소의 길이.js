9097af763b29a68c2213e8bfd4ecc78f742d085f
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-12-02 19:20:15 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -0,0 +1,12 @@
+function solution(strlist) {
+    return strlist.map(el => el.length)
+}
+
+// let answer = [];
+// //1. for(let i = 0; i < strlist.legnth; i++){
+//        answer.push(strlist[i].length)
+// }
+
+
+//map(el => el.length) 하나하나에 특정한 작업을 걸어줄거라면  map을 쓴다.
+//filter 특정하게 걸러준다
