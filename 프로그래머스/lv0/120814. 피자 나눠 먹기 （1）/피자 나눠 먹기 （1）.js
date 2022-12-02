ee3312b37abc83432e3d775b75695906f4dfa031
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-12-02 17:59:58 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -13,3 +13,14 @@ function solution(n) {
 
     return n % 7 === 0 ? Math.floor(n /7) : Math.floor(n / 7) +1
 }
+
+function solution(n) {
+
+    return Math.ceil(n/7)
+}
+
+// 소수점이 남았을때 올려줘 / 버려줘는 +1
+
+//목 n 7 조각 n/7 16 2.xxxxxx +1
+
+// 1 0.1xxxxdhffla =1 2.xxxxx
