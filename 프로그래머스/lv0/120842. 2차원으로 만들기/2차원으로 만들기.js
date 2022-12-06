5e4406604657f8a5152d7d556a645abe2de59d43
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2022-12-06 09:57:14 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -17,3 +17,12 @@ function solution(num_list, n) {
 
     return answer;
 }
+function solution(num_list, n) {
+    var answer = [];
+
+    while(num_list.length) {
+        answer.push(num_list.splice(0,n));
+    }
+
+    return answer;
+}
