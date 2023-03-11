c03bc79114cde08a61a426d95537e230566d94ea
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2023-03-12 03:40:58 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,11 +1,10 @@
-// function solution(num1, num2) {
-//     if (num1 === num2){
-//         return 1
-//     } else {
-//         return -1
-//     }
-// }
-
-let solution = (num1, num2) => num1-num2 !== 0 ? -1 : 1
-    // 비교식 ? 1번 : 2번
-    // t 1번 /f 2번
+function solution(num1, num2) {
+    var answer = 0;
+    
+    if (num1 === num2) {
+        answer = 1
+    } else {
+        answer = -1
+    }
+    return answer;
+}
