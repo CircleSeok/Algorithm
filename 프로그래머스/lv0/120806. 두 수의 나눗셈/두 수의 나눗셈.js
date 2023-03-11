a6692a6fcb511b070a4f2389c8c25846f38680f5
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2023-03-12 03:58:59 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -1,6 +1,3 @@
 function solution(num1, num2) {
-    let answer = Math.floor((num1 / num2) * 1000)
-    return answer
+return Math.floor((num1/num2)*1000)
 }
-
-// let solution = (num1, num2) = > Math.floor((num1/ num2) * 1000)
