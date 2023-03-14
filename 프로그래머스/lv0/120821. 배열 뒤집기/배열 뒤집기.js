91a14e568f7338868577cb513269c7f3316b6e78
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2023-03-15 02:04:53 +0900

    [level 0] Title: 배열 뒤집기, Time: 2.37 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,6 +1,17 @@
+// function solution(num_list) {
+//     var answer = num_list.reverse()
+//     return answer;
+// }
+
 function solution(num_list) {
-    var answer = num_list.reverse()
+    let answer = []
+    for(i = num_list.length; i>=1; i--){
+        answer.push(num_list[i-1])
+        console.log(num_list[i])
+    }
     return answer;
 }
 
-//배열을 뒤집으려면 for문으로 한번 돌아야할까?
+// 5
+
+//배열을 뒤집으려면 for문으로 한번 돌아야할까? reverse 함수를 쓰면 가능
