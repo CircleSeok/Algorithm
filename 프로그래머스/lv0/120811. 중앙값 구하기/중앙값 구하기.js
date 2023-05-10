dfc5b65477229828d42ead40c3d3939491ce68ee
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2023-05-10 23:32:24 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,42 +1,10 @@
 function solution(array) {
-   
-    
-    let arrayI = 0;
-    let newArray = []
-    while(arrayI < array.length) {
-            let minNum = 1000;
-    let i = 0;
-    
-    while(i< array.length){
-        if(minNum > array[i]){
-            minNum = array[i];
-        }
-        i = i +1
-    }
-    // minNum = 최소값
-    newArray.push(minNum)
-    
-    let i2 = 0;
-    while(i2< array.length){
-        if(minNum === array[i2]) {
-         array[i2] = 1000;
-            break;
-        }
-        i2= i2 + 1
-    }
-        arrayI = arrayI + 1
-    }
-    return newArray[Math.floor(array.length/2)]
-    console.log('newArray', newArray);
+  // 배열 길이 중 중앙값 (중간 index) 찾기
+  const mid_num = Math.floor(array.length / 2);
+
+  // 오름차순으로 정렬하기 - sort() 메서드 사용
+  const asc_array = array.sort((a, b) => a - b);
+
+  // 오름차순된 새로운 배열 중 중간 index 반환하기
+  return asc_array[mid_num];
 }
-
-//정렬
-//배열에서 제일 작은 값을 찾는다
-//찾으면 새 배열에 넣기
-//원래 배열에 찾은 값은 지운다.
-//만약 원래 배열 길이만큼 반복했으면 끝낸다. 아니면 배열중 가장 작은 값을 찾는다로 반복
-//가운데 값 꺼내기
-//배열길이 [Math.floor(n/2)]
-
-
-
