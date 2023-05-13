1449796d80cf779b9c755f3c44ec2428623ea120
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2023-05-13 22:28:20 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,26 +1,11 @@
-// function solution(n) {
-//     var answer = [];
-//     for(let i = 0; i <= n ; i ++){
-//        if(i%2 === 1){
-//         answer.push(i)   
-//        }
-//     }  
-//     console.log(answer)
-//     return answer;
-// }
-
 function solution(n) {
     var answer = [];
-    let i = 0;
-    while(i <= n) {
+    
+    for(let i = 1; i <= n; i++) {
         if(i % 2 === 1) {
-            answer.push(i)
+            answer.push(i);
         }
-        i ++
     }
+    
     return answer;
 }
-
-// 입력값만큼 반복문 실행
-// ex)10 [1,2,3,4,5,6,7,8,9,10]
-// 받은 수에서 %2 해서 남은게 1이면 홀수이니 그것만 받아서  answer에 넣기
