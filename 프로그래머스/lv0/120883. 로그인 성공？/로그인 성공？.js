c5f2890e1c476a11d9cf2f8cf1af38e362b28fa6
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2023-07-27 17:33:26 +0900

    [level 0] Title: 로그인 성공?, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,9 +1,8 @@
 function solution(id_pw, db) {
-  let result = '';
-  
-  db.map((item, i) => {if(id_pw[0] === item[0] && id_pw[1] === item[1]) result = "login"
-	else if(id_pw[0] === item[0] && id_pw[1] !== item[1]) result =  "wrong pw"
-	else if(id_pw[0] !== item[0] && id_pw[1] !== item[1]) result = "fail"})
-	
-  return result
+    const [ userId, userPw ] = id_pw;
+    for (const [ dbId, dbPw ] of db) {
+        if (userId === dbId && userPw === dbPw) return "login";
+        else if (userId === dbId && userPw !== dbPw) return "wrong pw";
+    }
+    return "fail";
 }
