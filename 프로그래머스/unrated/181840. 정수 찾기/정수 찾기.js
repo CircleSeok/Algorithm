7867675e1033d7b643e35b80ad3e2abddd161a50
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2023-12-03 02:42:11 +0900

    [level unrated] Title: 정수 찾기, Time: 0.02 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
@@ -5,3 +5,13 @@ if(num_list.includes(n)){
     return 0
 }
 }
+
+// function solution(num_list, n) {
+//     for (let i = 0; i < num_list.length; i++) {
+//         if (num_list[i] === n) {
+//             return 1; 
+//         }
+//     }
+    
+//     return 0; 
+// }
