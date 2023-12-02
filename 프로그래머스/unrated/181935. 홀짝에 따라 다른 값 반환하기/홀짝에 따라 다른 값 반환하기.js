b620c2dc38542b7f9fd60f47d58d1e5fc44d4fc5
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2023-12-03 02:55:36 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -3,12 +3,10 @@ function solution(n) {
 
     if (n % 2 === 1) {
         for (let i = 1; i <= n; i += 2) {
-            console.log(`현재 i 값(홀수): ${i}`);
             sum += i;
         }
     } else {
         for (let i = 2; i <= n; i += 2) {
-            console.log(`현재 i 값(짝수): ${i}`);
             sum += i * i;
         }
     }
