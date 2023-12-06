57d91824d1c2c59671e2b0f60675fd7209eae46f
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2023-12-06 13:59:24 +0900

    [level unrated] Title: flag에 따라 다른 값 반환하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.js
@@ -0,0 +1,12 @@
+// function solution(a, b, flag) {
+// if(flag === false) {
+//     return a - b
+// } else {
+//     return a + b
+// }
+// }
+
+
+function solution(a, b, flag) {
+return flag ? a + b : a - b
+}
