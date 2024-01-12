e66a913b0fff5e0c41dd63303c9213a5ca4a3565
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2024-01-12 16:32:35 +0900

    [level Lv.0] Title: 주사위 게임 1, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,16 @@
+function solution(a, b) {
+    let score = 0;
+
+    if (a % 2 === 1 && b % 2 === 1) {
+      
+        score = a ** 2 + b ** 2;
+    } else if (a % 2 === 1 || b % 2 === 1) {
+       
+        score = 2 * (a + b);
+    } else {
+       
+        score = Math.abs(a - b);
+    }
+
+    return score;
+}
