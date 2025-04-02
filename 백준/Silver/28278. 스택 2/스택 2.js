9927d80205d4b92f95f44a9953c5d4c1800693f8
CircleSeok <107212281+CircleSeok@users.noreply.github.com>
2025-04-02 23:29:10 +0900

    [Silver IV] Title: 스택 2, Time: 968 ms, Memory: 173716 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.js
@@ -0,0 +1,42 @@
+const [N,...arr] = require("fs").readFileSync("/dev/stdin")
+.toString()
+.trim()
+.split("\n");
+
+const stack = []; 
+let results = ""; // 2. 문자열로 두었을 때
+
+for (let i = 0; i < arr.length; i++) {
+  let query = arr[i];
+  let [cmd, value] = query.split(" ");
+
+  switch (cmd) {
+    case "1":
+      stack.push(parseInt(value));
+      break;
+    case "2":
+      if (stack.length > 0) {
+        results += `${stack.pop()}\n`;
+      } else {
+        results += `-1\n`;
+      }
+      break;
+    case "3":
+      results += `${stack.length}\n`;
+      break;
+    case "4":
+      results += stack.length === 0 ? `1\n` : `0\n`;
+      break;
+    case "5":
+      if (stack.length > 0) {
+        results += `${stack[stack.length - 1]}\n`;
+      } else {
+        results += `-1\n`
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+console.log(results);
